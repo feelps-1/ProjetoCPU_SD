c8ab54539f096d93ed33e9137e617fe92824d3eb
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-07-17 20:16:12 -0300

    Update proporcao.py

--- proporcao.py
+++ proporcao.py
@@ -1,11 +1,10 @@
-w = int(input())
-h = int(input())
+w = 1920
+h = 1080
 
-change = float(input())
-
-newW = w * change
-newH = h * change
-
-print(f"""A largura original da foto era {w} e a altura original era {h}.\n
-          O tamanho foi mudado por um fator de {change}.\n
-          As novas medidas são {newW} e {newH}""")
+def prop(width, height):
+    if width * 9 == height * 16:
+        return 1
+    elif width * 3 == height * 4:
+        return 2
+    else:
+        return 0
